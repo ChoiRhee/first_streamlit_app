d294af502353d89a5dd71844e68981228e1e724b
최리 <65180076+ChoiRhee@users.noreply.github.com>
2022-10-26 17:55:34 +0900

    Update streamlit_app.py
    added multi select box

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,13 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# pandas 설치 없이 사용 가능 (스트림릿에 포함되어 있음)
 import pandas as pd
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+#streamlit.dataframe(my_fruit_list)
+
+# 사용자가 상호작용을 할 수 있도록 다중 선택이라는 사용자 대화형 위젯 추가
+streamlit.multiselect('Pick some fruits:", list(my_fruit_list.index))
+
+# 페이지에 table 보여주기
 streamlit.dataframe(my_fruit_list)
