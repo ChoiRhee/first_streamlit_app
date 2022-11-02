88413546a130093a1cbff99731799cb63e160e29
최리 <65180076+ChoiRhee@users.noreply.github.com>
2022-11-02 16:16:17 +0900

    Update streamlit_app.py
    stop 아래로 옮김

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-# SNOWFLAKE에 원할 때만 데이터를 전송할 수 있도록 STOP 설정
-streamlit.stop()
-
 streamlit.header('The fruit load list contains:')
 # Snowflake-related functions
 def get_fruit_load_list():
@@ -58,6 +55,8 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets['snowflake'])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
 
 # allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
