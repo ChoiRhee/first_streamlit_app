769d4b28034734972e9d7a95051f6333dec64ac7
최리 <65180076+ChoiRhee@users.noreply.github.com>
2022-11-02 15:48:55 +0900

    Update streamlit_app.py
    make another multiselect box

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,8 @@ my_cur.execute('SELECT * FROM FRUIT_LOAD_LIST')
 my_data_rows = my_cur.fetchall()
 streamlit.header('The fruit load list contains:')
 streamlit.dataframe(my_data_rows)
+
+# allow the end user to add a fruit to the list
+add_my_fruit = streamlit.multiselect('What fruit would you like to add?', list(my_fruit_list.index), ['jackfruit'])
+added_fruits_show = add_my_fruit.loc[add_my_fruit]
+streamlit.write('Thanks for adding', add_my_fruit)
