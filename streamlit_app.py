e7a0db277f1d71d412358d8b2d36361efa82ce21
최리 <65180076+ChoiRhee@users.noreply.github.com>
2022-11-02 11:25:38 +0900

    Update streamlit_app.py
    normalize json view

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,9 @@ streamlit.header('Fruityvice Fruit Advice')
 
 import requests
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # 그냥 json 형태로 보여줌
+
+# json normalization
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# 테이블 형태로 보여주기
+streamlit.dataframe(fruityvice_normalized)
